2166fa0915e671cb7df0e7be7041801ee3f4d5e5
Samuel Ding <samuel.ding.nz@gmail.com>
2021-09-14 23:52:43 +0200

    handle HS4 & HS6

--- chart.js
+++ chart.js
@@ -5,19 +5,19 @@ const fs = require('fs')
 const fetch = require('node-fetch');
 
 
-
-function drawTreemap(countryName, countryISO2, oecCode, im, titleOffset, data, width, height) {
+function drawTreemap(countryName, countryISO2, oecCode, HSx, im, titleOffset, data, netPort) {
   // 1. defining data
-  const _id = `${countryISO2}_nt${im>0?'im':'ex'}`;
-  const _titleSize = Math.ceil(Math.max(10, width/100));
-  const _value = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(width*height/1000)+' B'
+  const [width, height] = getWidthHeight(netPort);
+  const _id = `${countryISO2.replace(/\W/g,'_')}_nt${im>0?'im':'ex'}`;
+  const _titleSize = Math.ceil(Math.max(11, width/100));
+  const _value = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(netPort/1000)+' M'
 
   // 2. defining mapping
   const mapping = {
-    hierarchy: { value: ["Section ID", "HS2 ID", "HS4 ID"] },
+    hierarchy: { value: ["Section ID", "HS2 ID", `${HSx} ID`] },
     size: { value: "Trade Value" },
     color: { value: "Section" },
-    label: { value: ["HS4", "Trade Value"] }
+    label: { value: [HSx, "Trade Value"] }
   };
   /**
    * // Fix bug - aggregation iterater over string  'c', 's', 'v' ... , add following line to \node_modules\@rawgraphs\rawgraphs-core\lib\index.cjs.js:4098 
@@ -38,28 +38,41 @@ function drawTreemap(countryName, countryISO2, oecCode, im, titleOffset, data, w
   // 5. rendering to the DOM
   const svg = viz.renderToString(new JSDOM(``).window.document)
     .replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"')
+    // add top-labels and offsets
     .replace('<g transform="translate(10,10)" id="viz"><g id="leaves">',`
-    <g transform="translate(10,${titleOffset})" id="${_id}_2020"><text dominant-baseline="text-before-edge" class="txt"><tspan x="20" y="0.2em" style="font-family: Arial, sans-serif; font-size: ${_titleSize}px; fill: black; font-weight: bold;">${countryName} net ${im>0?'import':'export'} ${_value}</tspan></text><g id="${_id}_2020_leaves" transform="translate(0, ${Math.ceil(10+(_titleSize-12)*1.4)})">`)
+    <g transform="translate(10,${titleOffset})" id="${_id}_2020"><text dominant-baseline="text-before-edge" class="txt"><tspan x="20" y="0.2em" style="font-family: Arial, sans-serif; font-size: ${_titleSize}px; fill: black; font-weight: bold;">${countryName} net ${im>0?'import':'export'} ${_value}</tspan></text><g id="${_id}_2020_leaves" transform="translate(0, ${Math.ceil(15+(_titleSize-11)*1.5)})">`)
+    // add global id
     .replace(/("(?:url\()?#?)((?:path|clip)\d+\)?")/g, `$1${_id}_$2`)
+    // remove extra style
+    .replace(/ style="font-family: Arial, sans-serif; font-size: 10px; fill: black; font-weight: bold;"/g, '')
+    // add <title> (tooltip on supported browsers)
+    .replace(/<tspan x="3" y="0\.2em">([^<>]+)<\/tspan><tspan x="3" y="1\.3em">(\d+)<\/tspan><\/text>/g, `<tspan x="3" y="0.2em">$1</tspan><tspan x="3" y="1.3em">$2</tspan></text><title>$1&#10;$2</title>`)
+    // remove <rect> with 0 width|height
+    .replace(/<g transform[^<]+<[^<]+(width|height)="0"[^&]+&#10;\d+<\/title><\/g>/g, '')
+    // remove <text> on small width|height < 10px
+    .replace(/(<g transform[^<]+<[^<]+(?:width|height)="\d"[^\/]+\/[^\/]+)(<clipPath [^#]+#[^#]+#[^#]+<\/text>)/g, '$1')
+    // remove verbose HS6 labels
+    .replace(/____[^<]+<\/tspan>/g, '</tspan>')
+    .replace(/____/g, '&#10;')
     .replace(/<\/svg>$/, '\n</svg>');
     // fs.writeFileSync(`./svg/${oecCode.slice(0,2)}_${oecCode.slice(2)}_${im>0?'im':'ex'}ports_${width*height}.svg`, svg);
   return svg;
 }
 
 
-async function getNetportData(im, oecCodes) {
-  const template = JSON.parse(fs.readFileSync('./template.json').toString());
+async function getNetportData(HSx, im, oecCodes) {
+  const template = JSON.parse(fs.readFileSync(`./template_${HSx}.json`).toString());
   for (const oecCode of oecCodes) {
-    const url = (port) => `https://oec.world/olap-proxy/data?cube=trade_i_baci_a_92&${port}er+Country=${oecCode}&drilldowns=HS4&measures=Trade+Value&parents=true&Year=2020&sparse=false&locale=en`;
+    const url = (port) => `https://oec.world/olap-proxy/data?cube=trade_i_baci_a_92&${port}er+Country=${oecCode}&drilldowns=${HSx}&measures=Trade+Value&parents=true&Year=2020&sparse=false&locale=en`;
     const importData = (await (await fetch(url('Import'))).json()).data;
     const exportData = (await (await fetch(url('Export'))).json()).data;
-    if (!importData || !exportData || !Array.isArray(importData) || !Array.isArray(importData)) continue;
-    
+    if (!importData || !exportData || !Array.isArray(importData) || !Array.isArray(exportData) || importData.length == 0 || exportData.length == 0) continue;
+    console.log(oecCode,im)
     template.forEach(t => {
-      const hs4id = t['HS4 ID']
-      const importValue = importData.find(i => i['HS4 ID'] == hs4id)?.['Trade Value'] || 0;
-      const exportValue = exportData.find(i => i['HS4 ID'] == hs4id)?.['Trade Value'] || 0;
-      const value = im * (importValue-exportValue)/1000000; // in million
+      const hsxid = t[`${HSx} ID`]
+      const importValue = importData.find(i => i[`${HSx} ID`] == hsxid)?.['Trade Value'] || 0;
+      const exportValue = exportData.find(i => i[`${HSx} ID`] == hsxid)?.['Trade Value'] || 0;
+      const value = im * (importValue-exportValue)/100000; // cent -> thousand$
       t['Trade Value'] += value;        
     })
   }
@@ -69,22 +82,20 @@ async function getNetportData(im, oecCodes) {
   template.forEach(t => {
     netPort += Math.max(0,t['Trade Value'])
     t['Trade Value'] = Math.round(Math.max(0,t['Trade Value']))
-    // skip 0 values, but keep all sections
+    // keep all sections to utilize full color palette
     if (t['Section ID'] > sectionId) {
       sectionId = t['Section ID'];
       netportData.push(t);
-    } else if (t['Trade Value'] >= 100) {
+    } else if (t['Trade Value'] >= 1000) {
       netportData.push(t);
     }
   })
-  const width = Math.round(Math.sqrt(netPort*2));
-  const height = Math.round(Math.sqrt(netPort/2));
-  fs.writeFileSync('./netports.json', JSON.stringify(netportData))
-  if (width == 0) return null;
-  return [netportData, width, height]
+
+  // fs.writeFileSync('./netports.json', JSON.stringify(netportData))
+  if (netPort == 0) return null;
+  return [netportData, netPort]
 }
 
-// getNetportData(1, [ 'aszhn', 'China', 'zh', 'zh' ]).then(d => drawTreemap(...d))
 async function makeMap(mapWidth, mapHeight) {
   let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${mapWidth}" height="${mapHeight}">
   <rect width="${mapWidth}" height="${mapHeight}" x="0" y="0" fill="#FFFFFF" id="backgorund"/>\n`;
@@ -131,24 +142,22 @@ async function makeMap(mapWidth, mapHeight) {
   fs.writeFileSync(`./worldtrademap.svg`, svg)
 }
 
-async function makeGroupMap(groupName) {
+async function makeGroupMap(HSx, groupName) {
   const oecCodes = [];
   const iso2s = [];
   const countryNames = [];
   for(const country of fs.readFileSync('./countries1.tsv').toString().split('\r\n').map(c=>c.split('\t'))){
     oecCodes.push(country[0]); iso2s.push(country[1]); countryNames.push(country[4]);
   }
-  netImport = await getNetportData( 1, oecCodes);
-  netExport = await getNetportData(-1, oecCodes);
-  const importWidth = netImport[1];
-  const importHeight = netImport[2];
-  const exportWidth = netExport[1];
-  const exportHeight = netExport[2];
+  netImport = await getNetportData(HSx, 1, oecCodes);
+  netExport = await getNetportData(HSx,-1, oecCodes);
+  const [importWidth, importHeight] = getWidthHeight(netImport[1]);
+  const [exportWidth, exportHeight] = getWidthHeight(netExport[1]);
   const importOffset = Math.ceil(importHeight/150);
-  const exportOffset = importHeight + Math.ceil(Math.max(10, (importWidth+exportWidth)/150-10)); // import height + export fontsize
+  const exportOffset = importHeight + Math.ceil(Math.max(10, (importWidth)/50-10)); // import height + export fontsize
   
-  const importSvgG = drawTreemap(groupName, groupName.replace(/\W/g,''), 'oecCode',  1, importOffset, ...netImport).split('\n')[1];
-  const exportSvgG = drawTreemap(groupName, groupName.replace(/\W/g,''), 'oecCode', -1, exportOffset, ...netExport).split('\n')[1];
+  const importSvgG = drawTreemap(groupName, groupName.replace(/\W/g,''), 'oecCode', HSx, 1, importOffset, ...netImport).split('\n')[1];
+  const exportSvgG = drawTreemap(groupName, groupName.replace(/\W/g,''), 'oecCode', HSx,-1, exportOffset, ...netExport).split('\n')[1];
   const mapWidth = Math.max(importWidth,exportWidth) + 20;
   const mapHeight = importOffset*4 + exportHeight + exportOffset + 20;
   let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${mapWidth}" height="${mapHeight}">
@@ -160,5 +169,13 @@ async function makeGroupMap(groupName) {
   fs.writeFileSync(`./group/${groupName}_trademap.svg`, svg)
 }
 
+function getWidthHeight(area) {
+  area = Math.ceil(area/10); // area 1px = 10k$
+  const ratio = 1.618034;
+  width  = Math.ceil(Math.sqrt(area*ratio));
+  height = Math.ceil(Math.sqrt(area/ratio));
+  return [width, height]
+}
+
 // makeMap(150000,60000)
-makeGroupMap(process.argv.slice(2).join(' '))
+makeGroupMap('HS6', process.argv.slice(2).join(' '))
